dc3eafa1351357248ccc4dd79606b39ffe914481
Sai <saiveerendra563@gmail.com>
2025-06-17 12:54:24 +0530

    fixed some errors and improved the responsiveness

--- resources/script.js
+++ resources/script.js
@@ -24,20 +24,27 @@ attemptsDisplay.textContent = `Attempts: 0 / 10`;
 // Event listener for submit button
 submit.addEventListener('click', () => {
   const guess = Number(input.value);
+
+  if (input.value === ''){
+    message.textContent = '😑 Please enter a number before submitting.';
+    message.style.color = 'black';
+    return;
+  } else if (guess > 100 || guess < 1){
+    message.textContent = '🙄 Please enter a number between 1 and 100.';
+    message.style.color = 'black';
+    return;
+  }
+
   attempts++;
   reset.style.display = 'inline-block';
 
-  if (guess > 100 || guess < 1){
-    message.textContent = '🙄 Please enter a number between 1 and 100.';
-    message.style.color = 'orange';
-  }
-  else if (guess === random) {
+  if (guess === random) {
     message.textContent = `🎉 Correct! The number was ${random}. You guessed it in ${attempts} attempts.`;
     message.style.color = 'green';
     input.disabled = true;
     submit.textContent = 'Play Again 😄';
     reset.style.display = 'none';
-    submit.addEventListener('click', () => location.reload());
+    submit.onclick = () => location.reload();
   }
   else if (attempts >= 10) {
     message.textContent = '😭 Game Over! You used all 10 attempts.';
@@ -45,7 +52,7 @@ submit.addEventListener('click', () => {
     input.disabled = true;
     reset.style.display = 'none';
     submit.textContent = 'Play Again 😁';
-    submit.addEventListener('click', () => location.reload());
+    submit.onclick = () => location.reload();
   }
   else if (guess < random) {
     message.textContent = '📉 Too low! Try again.';
